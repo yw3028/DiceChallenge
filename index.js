23b3129b649c6487973b4ac2c3f954377d80abf2
Yu <Yu@Yus-MacBook-Pro.local>
2020-03-15 20:46:47 +0100

    dice challenge

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var randomDiceImgSrc1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImgSrc2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomDiceImgSrc1);
+document.querySelector(".img2").setAttribute("src", randomDiceImgSrc2);
+
+var title = document.querySelector("h1");
+
+
+if (randomNumber1 === randomNumber2) {
+    title.innerHTML = "Draw!";
+} else if (randomNumber1 > randomNumber2) {
+    title.innerHTML = "🎉Player 1 Wins!";
+} else (randomNumber1 < randomNumber2) {
+    title.innerHTML = "🎉Player 2 Wins!";
+}
+
+
+
+
